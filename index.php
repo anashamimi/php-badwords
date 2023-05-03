edf147c7f5cd232e3a501319ba5c299edf974164
anashamimi <anashamimi2@gmail.com>
2023-05-03 16:28:04 +0200

    add phrase lenght

--- index.php
+++ index.php
@@ -4,15 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="request.php" method="POST">
-        <label for="phrase">Inserisci una frase</label>
-        <input type="text" name="phrase" id="phrase">
-        <label for="word">Inserisci una parola da censurare</label>
-        <input type="text" name="word" id="word">
-        <input type="submit" value="Invia">
-    </form>
+    <div>
+        <form action="request.php" method="POST">
+            <div>
+                <label for="phrase">Inserisci una frase</label>
+                <input type="text" name="phrase" id="phrase">
+            </div>
+            
+            <div>
+               <label for="word">Inserisci una parola da censurare</label>
+                <input type="text" name="word" id="word"> 
+            </div>
+            <div>
+                <input type="submit" value="Invia">
+            </div>
+            
+        </form>
+    </div>
+    
 </body>
 </html>
--- request.php
+++ request.php
@@ -1,10 +1,18 @@
 <?php
-    $phrase = trim($_POST['phrase']); 
+
+    if(empty($phrase = trim($_POST['phrase'])) !== true ){
+        $phrase = trim($_POST['phrase']); 
     $word = trim($_POST['word']);
 
     $lenght = strlen($phrase);
+    $phrase_no_space = str_replace(' ', '', $phrase);
+    $lenght_no_space = strlen($phrase_no_space);
 
-    $censured_phrase = str_replace($word, '****', $phrase)
+    $censured_phrase = str_replace($word, '***', $phrase);
+    } else {
+        $error = 'i dati non sono stati inseriti correttamente';
+    }
+    
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +24,10 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo $phrase . ' ' . $lenght ?></h1>
-    <h1><?php echo $censured_phrase ?></h1>
+    <h2><?php echo $error?></h2>
+    <h2><?php echo $phrase?></h2>
+    <p>Lunghezza con spazi: <?php echo $lenght ?></p>
+    <p>Lunghezza senza spazi: <?php echo $lenght_no_space ?></p>
+    <h2><?php echo $censured_phrase ?></h2>
 </body>
 </html>
